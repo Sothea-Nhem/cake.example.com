6cbe472406fbca19ee04cc97d198a9b8c0a38d58
Sothea Nhem <goddess.of.light.808@gmail.com>
2021-12-14 13:35:59 -0600

    Add Users CRUD

--- tests/Fixture/PostsFixture.php
+++ tests/Fixture/PostsFixture.php
@@ -19,15 +19,15 @@ class PostsFixture extends TestFixture
     {
         $this->records = [
             [
-                'id' => '2683f563-f372-4ecc-98f2-0e6e3197cd6d',
+                'id' => 'ad3118f8-8244-4a2c-89f3-85acd5d3e82b',
                 'title' => 'Lorem ipsum dolor sit amet',
                 'slug' => 'Lorem ipsum dolor sit amet',
                 'meta_keywords' => 'Lorem ipsum dolor sit amet',
                 'meta_description' => 'Lorem ipsum dolor sit amet',
                 'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-                'user_id' => '48c952eb-7098-4da9-86e9-db1e4eee9b37',
-                'created' => '2021-12-14 19:12:39',
-                'modified' => '2021-12-14 19:12:39',
+                'user_id' => 'c597d522-7154-47db-8989-eb3b65ef1992',
+                'created' => '2021-12-14 19:29:27',
+                'modified' => '2021-12-14 19:29:27',
             ],
         ];
         parent::init();
